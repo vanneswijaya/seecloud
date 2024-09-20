c9f605279bdf2030eed4f3a83840a1b192491736
vanneswijaya <vannesoey@gmail.com>
2024-09-20 20:10:11 +0800

    setup app shell layout

--- src/components/canvas.tsx
+++ src/components/canvas.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import { Stage, Layer, Image } from "react-konva";
 import useImage from "use-image";
 import { Stage as StageType } from "konva/lib/Stage";
+import { AppShell, Burger, Group, Skeleton } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { MantineLogo } from "@mantinex/mantine-logo";
 
 interface ImageType {
   id: string;
@@ -34,50 +37,74 @@ export default function Canvas() {
   const stageRef = React.useRef<StageType>(null);
   const [images, setImages] = React.useState<ImageType[]>([]);
   const [dragUrl, setDragUrl] = React.useState<string>("");
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-    <div>
-      Try to drag image into the stage:
-      <br />
-      <img
-        alt="lion"
-        src="https://konvajs.org/assets/lion.png"
-        draggable="true"
-        onDragStart={(e) => {
-          const targetImage = e.target as ImageTargetType;
-          setDragUrl(targetImage.src);
-        }}
-      />
-      <div
-        onDrop={(e) => {
-          e.preventDefault();
-          // register event position
-          stageRef.current?.setPointersPositions(e);
-          // add image
-          setImages(
-            images.concat([
-              {
-                ...stageRef.current?.getPointerPosition(),
-                src: dragUrl,
-                id: images.length.toString(),
-              },
-            ])
-          );
-        }}
-        onDragOver={(e) => e.preventDefault()}
-      >
-        <Stage
-          width={window.innerWidth}
-          height={window.innerHeight}
-          style={{ border: "1px solid grey" }}
-          ref={stageRef}
-        >
-          <Layer>
-            {images.map((image) => {
-              return <URLImage key={image.id} image={image} />;
-            })}
-          </Layer>
-        </Stage>
-      </div>
-    </div>
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <MantineLogo size={30} />
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>
+        <div>
+          Try to drag image into the stage:
+          <br />
+          <img
+            alt="lion"
+            src="https://konvajs.org/assets/lion.png"
+            draggable="true"
+            onDragStart={(e) => {
+              const targetImage = e.target as ImageTargetType;
+              setDragUrl(targetImage.src);
+            }}
+          />
+          <div
+            onDrop={(e) => {
+              e.preventDefault();
+              // register event position
+              stageRef.current?.setPointersPositions(e);
+              // add image
+              setImages(
+                images.concat([
+                  {
+                    ...stageRef.current?.getPointerPosition(),
+                    src: dragUrl,
+                    id: images.length.toString(),
+                  },
+                ])
+              );
+            }}
+            onDragOver={(e) => e.preventDefault()}
+          >
+            <Stage
+              width={window.innerWidth}
+              height={window.innerHeight}
+              style={{ border: "1px solid grey" }}
+              ref={stageRef}
+            >
+              <Layer>
+                {images.map((image) => {
+                  return <URLImage key={image.id} image={image} />;
+                })}
+              </Layer>
+            </Stage>
+          </div>
+        </div>
+      </AppShell.Main>
+    </AppShell>
   );
 }
