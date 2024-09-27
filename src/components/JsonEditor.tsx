16144cada6f4a31d072dd7df38b3af887c6724dc
vanneswijaya <vannesoey@gmail.com>
2024-09-27 14:59:44 +0800

    added monaco editor component for cf template

---
+++ src/components/JsonEditor.tsx
@@ -0,0 +1,5 @@
+import Editor from "@monaco-editor/react";
+
+export const JsonEditor = () => {
+  return <Editor height="90vh" defaultLanguage="json" />;
+};
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,7 +2,7 @@
 import { useState, useRef } from "react";
 import { Stage, Layer } from "react-konva";
 import { Stage as StageType } from "konva/lib/Stage";
-import { AppShell, Burger, Group, Card, Text, Flex } from "@mantine/core";
+import { AppShell, Burger, Group, Card, Text, Flex, Tabs } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { MantineLogo } from "@mantinex/mantine-logo";
 import {
@@ -15,6 +15,7 @@ import jsonData from "../common/master-map.json";
 import { getPoints } from "@/common/util";
 import { Layer as LayerType } from "konva/lib/Layer";
 import { Line } from "konva/lib/shapes/Line";
+import { JsonEditor } from "./JsonEditor";
 
 export default function Main() {
   const stageRef = useRef<StageType>(null);
@@ -74,109 +75,127 @@ export default function Main() {
         })}
       </AppShell.Navbar>
       <AppShell.Main>
-        <div>
-          Drag components into the stage:
-          <br />
-          <div
-            onDrop={(e) => {
-              e.preventDefault();
-              stageRef.current?.setPointersPositions(e);
-              draggedComponentType &&
-                setStageComponents(
-                  stageComponents.concat([
-                    {
-                      ...stageRef.current?.getPointerPosition(),
-                      componentType: draggedComponentType,
-                      id: stageComponents.length.toString(),
-                    },
-                  ])
-                );
-            }}
-            onDragOver={(e) => e.preventDefault()}
-          >
-            <Stage
-              width={window.innerWidth}
-              height={window.innerHeight}
-              style={{ border: "1px solid grey" }}
-              ref={stageRef}
-              onClick={() => setActiveStageComponentIndex("")}
-            >
-              <Layer ref={layerRef}>
-                {stageComponents.map((props) => {
-                  return (
-                    <StageComponent
-                      key={props.id}
-                      props={props}
-                      isActive={activeStageComponentIndex === props.id}
-                      componentPendingConnect={pendingConnect}
-                      onActivate={() => setActiveStageComponentIndex(props.id)}
-                      onDelete={() => {
-                        setStageComponents(
-                          stageComponents.filter(
-                            (stageComponent) => stageComponent.id !== props.id
-                          )
-                        );
-                        const newConnectors: Connector[] = [];
-                        connectors.forEach((connector) => {
-                          if (
-                            connector.from === props.id ||
-                            connector.to === props.id
-                          ) {
-                            layerRef.current
-                              ?.findOne("#" + connector.id)
-                              ?.destroy();
-                          } else {
-                            newConnectors.push(connector);
+        <Tabs variant="pills" defaultValue="canvas">
+          <Tabs.List>
+            <Tabs.Tab value="canvas">Canvas</Tabs.Tab>
+            <Tabs.Tab value="template">Template</Tabs.Tab>
+          </Tabs.List>
+          <Tabs.Panel value="canvas">
+            <div>
+              Drag components into the stage:
+              <br />
+              <div
+                onDrop={(e) => {
+                  e.preventDefault();
+                  stageRef.current?.setPointersPositions(e);
+                  draggedComponentType &&
+                    setStageComponents(
+                      stageComponents.concat([
+                        {
+                          ...stageRef.current?.getPointerPosition(),
+                          componentType: draggedComponentType,
+                          id: stageComponents.length.toString(),
+                        },
+                      ])
+                    );
+                }}
+                onDragOver={(e) => e.preventDefault()}
+              >
+                <Stage
+                  width={window.innerWidth}
+                  height={window.innerHeight}
+                  style={{ border: "1px solid grey" }}
+                  ref={stageRef}
+                  onClick={() => setActiveStageComponentIndex("")}
+                >
+                  <Layer ref={layerRef}>
+                    {stageComponents.map((props) => {
+                      return (
+                        <StageComponent
+                          key={props.id}
+                          props={props}
+                          isActive={activeStageComponentIndex === props.id}
+                          componentPendingConnect={pendingConnect}
+                          onActivate={() =>
+                            setActiveStageComponentIndex(props.id)
                           }
-                        });
-                        setConnectors(newConnectors);
-                      }}
-                      onConnect={() => setPendingConnect(props)}
-                      onDragMove={() => {
-                        connectors.forEach((connector) => {
-                          const line: Line | undefined =
-                            layerRef.current?.findOne("#" + connector.id);
-                          const fromNode = layerRef.current?.findOne(
-                            "#" + connector.from
-                          );
-                          const toNode = layerRef.current?.findOne(
-                            "#" + connector.to
-                          );
-                          line?.points(
-                            getPoints(fromNode ?? null, toNode ?? null)
-                          );
-                        });
-                      }}
-                      onConfirmConnect={() => {
-                        setPendingConnect(null);
-                        setActiveStageComponentIndex("");
-                        const newLine = new Line({
-                          stroke: "black",
-                          id: "line" + connectors.length.toString(),
-                        });
-                        layerRef.current?.add(newLine);
-                        newLine.points(
-                          getPoints(
-                            layerRef.current?.findOne("#" + props.id) ?? null,
-                            layerRef.current?.findOne(
-                              "#" + pendingConnect?.id
-                            ) ?? null
-                          ) ?? [0, 0, 0, 0]
-                        );
-                        const newConnector: Connector = {
-                          id: newLine.id(),
-                          from: props.id,
-                          to: pendingConnect?.id,
-                        };
-                        setConnectors(connectors.concat([newConnector]));
-                      }}
-                    />
-                  );
-                })}
-              </Layer>
-            </Stage>
-          </div>
-        </div>
+                          onDelete={() => {
+                            setStageComponents(
+                              stageComponents.filter(
+                                (stageComponent) =>
+                                  stageComponent.id !== props.id
+                              )
+                            );
+                            const newConnectors: Connector[] = [];
+                            connectors.forEach((connector) => {
+                              if (
+                                connector.from === props.id ||
+                                connector.to === props.id
+                              ) {
+                                layerRef.current
+                                  ?.findOne("#" + connector.id)
+                                  ?.destroy();
+                              } else {
+                                newConnectors.push(connector);
+                              }
+                            });
+                            setConnectors(newConnectors);
+                          }}
+                          onConnect={() => setPendingConnect(props)}
+                          onDragMove={() => {
+                            connectors.forEach((connector) => {
+                              const line: Line | undefined =
+                                layerRef.current?.findOne("#" + connector.id);
+                              const fromNode = layerRef.current?.findOne(
+                                "#" + connector.from
+                              );
+                              const toNode = layerRef.current?.findOne(
+                                "#" + connector.to
+                              );
+                              line?.points(
+                                getPoints(fromNode ?? null, toNode ?? null)
+                              );
+                            });
+                          }}
+                          onConfirmConnect={() => {
+                            setPendingConnect(null);
+                            setActiveStageComponentIndex("");
+                            const newLine = new Line({
+                              stroke: "black",
+                              id: "line" + connectors.length.toString(),
+                            });
+                            layerRef.current?.add(newLine);
+                            newLine.points(
+                              getPoints(
+                                layerRef.current?.findOne("#" + props.id) ??
+                                  null,
+                                layerRef.current?.findOne(
+                                  "#" + pendingConnect?.id
+                                ) ?? null
+                              ) ?? [0, 0, 0, 0]
+                            );
+                            const newConnector: Connector = {
+                              id: newLine.id(),
+                              from: props.id,
+                              to: pendingConnect?.id,
+                            };
+                            setConnectors(connectors.concat([newConnector]));
+                          }}
+                        />
+                      );
+                    })}
+                  </Layer>
+                </Stage>
+              </div>
+            </div>
+          </Tabs.Panel>
+          <Tabs.Panel value="template">
+            <div>
+              <br />
+              <JsonEditor></JsonEditor>
+            </div>
+          </Tabs.Panel>
+        </Tabs>
       </AppShell.Main>
     </AppShell>
   );
