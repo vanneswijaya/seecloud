990588a573b0357dfee109f1feba49c1815090c9
vanneswijaya <vannesoey@gmail.com>
2024-11-02 19:10:13 +0800

    add successful example octokit POST request

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,19 +14,18 @@ app.get("/", async (req: Request, res: Response) => {
   const {
     data: { login },
   } = await octokit.rest.users.getAuthenticated();
-  console.log("Hi, %s", login);
-  res.send("Express");
+  res.send("Hi, " + login);
 });
 
-// app.get("/test-octokit", async (req: Request, res: Response) => {
-//   await octokit.request("POST /repos/{owner}/{repo}/issues", {
-//     owner: "octocat",
-//     repo: "Spoon-Knife",
-//     title: "Created with the REST API",
-//     body: "This is a test issue created by the REST API",
-//   });
-//   res.send("Express");
-// });
+app.get("/create-issue", async (req: Request, res: Response) => {
+  const result = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+    owner: "vanneswijaya",
+    repo: "seecloud",
+    title: "Created with the REST API",
+    body: "This is a test issue created by the REST API",
+  });
+  res.send(`Issue created: ${result}`);
+});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
