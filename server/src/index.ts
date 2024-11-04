369a178348fac2d34b1d7e66ac0f2265e372834e
vanneswijaya <vannesoey@gmail.com>
2024-11-04 17:48:29 +0800

    install and use cors in express

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,14 +2,15 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import controllers from "./controllers";
 import config from "./config";
+import cors from "cors";
 
 dotenv.config();
 
 const app: Express = express();
 const port = config.port;
 
+app.use(cors());
 app.use(express.json());
-
 app.use(controllers());
 
 app.listen(port, () => {
