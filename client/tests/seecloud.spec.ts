bd59f0450caa46d12389c9f6a10b79169cbdd168
vanneswijaya <vannesoey@gmail.com>
2025-02-25 00:31:01 +0800

    added more playwright test skeletons

--- client/tests/seecloud.spec.ts
+++ client/tests/seecloud.spec.ts
@@ -4,9 +4,28 @@ test.beforeEach(async ({ page }) => {
   await page.goto("/");
 });
 
-test("drag stage component from components menu to canvas", async ({
-  page,
-}) => {
-  await page.getByTestId("burger").click();
-  await page.getByText("IAM User").dragTo(page.getByTestId("canvas"));
+test.describe("Canvas Interactions", () => {
+  test("drag stage component from components menu to canvas", async ({
+    page,
+  }) => {
+    await page.getByTestId("burger").click();
+    await page.getByText("IAM User").dragTo(page.getByTestId("canvas"));
+    await expect(page.getByTestId("canvas")).toHaveText(
+      "DetailsDeleteConnectIAM UserUser0"
+    );
+  });
+
+  test("connect two stage components together", async ({ page }) => {
+    await page.getByTestId("burger").click();
+    await page.getByText("IAM User").dragTo(page.getByTestId("canvas"));
+    await expect(page.getByTestId("canvas")).toHaveText(
+      "DetailsDeleteConnectIAM UserUser0"
+    );
+  });
 });
+
+test.describe("Template Code", () => {});
+test.describe("Pull Request Generator", () => {});
+test.describe("Commit Generator", () => {});
+test.describe("Version History", () => {});
+test.describe("Access Analyzer", () => {});
