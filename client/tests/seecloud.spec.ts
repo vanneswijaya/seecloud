1c9ab4f5b618f5ce3d4514d1fc470e568f701d6f
vanneswijaya <vannesoey@gmail.com>
2025-02-25 23:57:36 +0800

    generalized test for template code

--- client/tests/seecloud.spec.ts
+++ client/tests/seecloud.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, type Page } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("/");
@@ -43,11 +43,9 @@ test.describe("Canvas Interactions", () => {
       page,
     }) => {
       await page.getByTestId("burger").click();
-      await page
-        .getByText(connection[0])
-        .dragTo(page.getByTestId("canvas"), {
-          targetPosition: { x: 30, y: 30 },
-        });
+      await page.getByText(connection[0]).dragTo(page.getByTestId("canvas"), {
+        targetPosition: { x: 30, y: 30 },
+      });
       await page.getByText(connection[1]).dragTo(page.getByTestId("canvas"), {
         targetPosition: { x: 200, y: 200 },
       });
@@ -67,13 +65,21 @@ test.describe("Canvas Interactions", () => {
 });
 
 test.describe("Template Code", () => {
-  test("add new stage component", async ({ page }) => {
-    await page.getByTestId("burger").click();
-    await page
-      .getByText("IAM User")
-      .dragTo(page.getByTestId("canvas"), { targetPosition: { x: 30, y: 30 } });
-    await page.getByRole("tab", { name: "Template" }).click();
-    await expect(page.locator(".monaco-editor")).toHaveText(/User0/);
+  stageComponentsForTest.forEach((stageComponent) => {
+    if (stageComponent.includes("IAM")) {
+      test(`add new stage component (${stageComponent})`, async ({ page }) => {
+        await page.getByTestId("burger").click();
+        await page
+          .getByText(stageComponent)
+          .dragTo(page.getByTestId("canvas"), {
+            targetPosition: { x: 30, y: 30 },
+          });
+        await page.getByRole("tab", { name: "Template" }).click();
+        await expect(page.locator(".monaco-editor")).toHaveText(
+          new RegExp(stageComponent.replace("IAM ", "").replace(" ", ""))
+        );
+      });
+    }
   });
 });
 
